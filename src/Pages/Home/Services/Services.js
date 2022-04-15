1223963c292f7d4fe69f7339dd97e53a896c30da
H_A _Opu <wwwhasib4@gmail.com>
2022-04-16 03:21:42 +0600

    map the services and give props on service compo

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
@@ -10,6 +11,9 @@ const Services = () => {
   return (
     <div>
       <h1>Services: {services.length}</h1>
+      {services.map((service) => (
+        <Service key={service.id} service={service}></Service>
+      ))}
     </div>
   );
 };
