b0b4afce691779aeefb1adcbb38c61085fb16b4f
H_A _Opu <wwwhasib4@gmail.com>
2022-04-28 12:15:02 +0600

    using axios.get and removing fetch at useServices hook

--- src/Hooks/useServices/useServices.js
+++ src/Hooks/useServices/useServices.js
@@ -1,11 +1,14 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const useServices = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/service")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
+    (async () => {
+      const { data } = await axios.get(`http://localhost:5000/service`);
+
+      setServices(data);
+    })();
   }, []);
   return [services, setServices];
 };
